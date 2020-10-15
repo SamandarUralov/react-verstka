3ae045f468c55dce48d677c6509ffc24a17c9774
SamandarUralov <uralovsamandar1999@gmail.com>
2020-10-15 15:58:35 +0500

    built

---
+++ build/precache-manifest.10db2b585b46e3e77ea1d7e71e3d648f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c55618f91ef2c7b802bbdb748e1bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58fc8a08f84968ac93c2",
+    "url": "/static/css/2.8ac7b331.chunk.css"
+  },
+  {
+    "revision": "63206eb9be4bda8c9e8e",
+    "url": "/static/css/main.06053186.chunk.css"
+  },
+  {
+    "revision": "58fc8a08f84968ac93c2",
+    "url": "/static/js/2.55770b85.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.55770b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63206eb9be4bda8c9e8e",
+    "url": "/static/js/main.7382bcaf.chunk.js"
+  },
+  {
+    "revision": "7c19bb87c74ff350013b",
+    "url": "/static/js/runtime-main.f19e9899.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2f46dc407ae7c1e109db697ebf093048",
+    "url": "/static/media/creative.2f46dc40.png"
+  },
+  {
+    "revision": "8886fe58e0586aea9620ad85a22cdfe1",
+    "url": "/static/media/image1.8886fe58.png"
+  },
+  {
+    "revision": "dae288b57ae4d4a7991fe12fd730e681",
+    "url": "/static/media/login_img.dae288b5.png"
+  },
+  {
+    "revision": "814e1e92a4f29a5b621e784665f9c177",
+    "url": "/static/media/offer.814e1e92.png"
+  },
+  {
+    "revision": "c767d6eb43d31832f56d7e3be1b268cd",
+    "url": "/static/media/refresh2.c767d6eb.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
